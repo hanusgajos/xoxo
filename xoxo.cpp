d39a0d85739129e81e58a4ae8d98852029c92841
Hanna Gajewska <borsulina@gmail.com>
2022-11-02 23:20:28 +0100

    update

--- xoxo.cpp
+++ xoxo.cpp
@@ -1,28 +1,67 @@
 #include <iostream>
 
-char a = ' ';
-char b = ' ';
-char c = ' ';
-char d = ' ';
-char e = ' ';
-char f = ' ';
-char g = ' ';
-char h = ' ';
-char i = ' ';
+char a[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+int x;
+int o;
 
 int main(int, char **)
 {
+    // start here to show board
     std::cout << "1 | 2 | 3" << std::endl;
     std::cout << "----------" << std::endl;
     std::cout << "4 | 5 | 6" << std::endl;
     std::cout << "----------" << std::endl;
     std::cout << "7 | 8 | 9" << std::endl;
+    for (char c : a)
+    {
 
-    std::cout << a << " | " << b << " | " << c << std::endl;
-    std::cout << "----------" << std::endl;
-    std::cout << a << " | " << b << " | " << c << std::endl;
-    std::cout << "----------" << std::endl;
-    std::cout << a << " | " << b << " | " << c << std::endl;
+        std::cout << "Player 1's turn:" << std::endl;
+        std::cout << a[0] << " | " << a[1] << " | " << a[2] << std::endl;
+        std::cout << "----------" << std::endl;
+        std::cout << a[3] << " | " << a[4] << " | " << a[5] << std::endl;
+        std::cout << "----------" << std::endl;
+        std::cout << a[6] << " | " << a[7] << " | " << a[8] << std::endl;
+        std::cin >> x;
+        if (a[x - 1] != ' ')
+        {
+            std::cerr << "taken" << std::endl;
+            return false;
+        }
+        if (!(x >= 1 && x <= 9))
+        {
+            std::cerr << "wrong input" << std::endl;
+            return false;
+        }
+        else
+        {
+
+            a[x - 1] = 'X';
+        }
+
+        std::cout << "Player 2's turn:" << std::endl;
+        std::cout << a[0] << " | " << a[1] << " | " << a[2] << std::endl;
+        std::cout << "----------" << std::endl;
+        std::cout << a[3] << " | " << a[4] << " | " << a[5] << std::endl;
+        std::cout << "----------" << std::endl;
+        std::cout << a[6] << " | " << a[7] << " | " << a[8] << std::endl;
+        std::cin >> o;
+
+        if (a[o - 1] != ' ')
+        {
+            std::cerr << "taken" << std::endl;
+            return false;
+        }
+        if (!(o >= 1 && o <= 9))
+        {
+            std::cerr << "wrong input" << std::endl;
+            return false;
+        }
+        else
+        {
+
+            a[o - 1] = 'O';
+        }
+    }
     //   std::cout << "\x1b[1;mHell" << std::endl;
 
     //   for (int i = 0; i < 256; i++)
