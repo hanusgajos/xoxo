bbd8fdf6804104fb52b5e9e2c14dd9426d302047
Hanna Gajewska <borsulina@gmail.com>
2022-11-03 12:58:55 +0100

    checking win

--- xoxo.cpp
+++ xoxo.cpp
@@ -1,12 +1,97 @@
 #include <iostream>
 
 char a[9] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-int x;
-int o;
+int x = 0;
+int o = 0;
+
+bool winx()
+{
+    std::cout << "tgtggttg" << std::endl;
+
+    if (a[0] == 'X' && a[1] == 'X' && a[2] == 'X')
+    {
+        return true;
+    }
+    else if (a[0] == 'X' && a[6] == 'X' && a[3] == 'X')
+    {
+        return true;
+    }
+    else if (a[0] == 'X' && a[4] == 'X' && a[8] == 'X')
+    {
+        return true;
+    }
+    else if (a[6] == 'X' && a[4] == 'X' && a[2] == 'X')
+    {
+        return true;
+    }
+    else if (a[3] == 'X' && a[4] == 'X' && a[5] == 'X')
+    {
+        return true;
+    }
+    else if (a[1] == 'X' && a[4] == 'X' && a[7] == 'X')
+    {
+        return true;
+    }
+    else if (a[6] == 'X' && a[7] == 'X' && a[8] == 'X')
+    {
+        return true;
+    }
+    else if (a[2] == 'X' && a[5] == 'X' && a[8] == 'X')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool wino()
+{
+    if (a[0] == 'O' && a[1] == 'O' && a[2] == 'O')
+    {
+        return true;
+    }
+    else if (a[0] == 'O' && a[6] == 'O' && a[3] == 'O')
+    {
+        return true;
+    }
+    else if (a[0] == 'O' && a[4] == 'O' && a[8] == 'O')
+    {
+        return true;
+    }
+    else if (a[6] == 'O' && a[4] == 'O' && a[2] == 'O')
+    {
+        return true;
+    }
+    else if (a[3] == 'O' && a[4] == 'O' && a[5] == 'O')
+    {
+        return true;
+    }
+    else if (a[1] == 'O' && a[4] == 'O' && a[7] == 'O')
+    {
+        return true;
+    }
+    else if (a[6] == 'O' && a[7] == 'O' && a[8] == 'O')
+    {
+        return true;
+    }
+    else if (a[2] == 'O' && a[5] == 'O' && a[8] == 'O')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 int main(int, char **)
 {
     // start here to show board
+    // modyfying regular boaard to make it a "board game"
+    // best of three wins, giving points after each round
+    // i didnt have time to make a new board game, writing this on thursday, going to mke it point based due to having multiple rounds
     std::cout << "1 | 2 | 3" << std::endl;
     std::cout << "----------" << std::endl;
     std::cout << "4 | 5 | 6" << std::endl;
@@ -25,17 +110,30 @@ int main(int, char **)
         if (a[x - 1] != ' ')
         {
             std::cerr << "taken" << std::endl;
-            return false;
+            std::cin >> x;
+            continue;
         }
         if (!(x >= 1 && x <= 9))
         {
             std::cerr << "wrong input" << std::endl;
-            return false;
+            std::cin >> x;
+            continue;
         }
+
         else
         {
 
             a[x - 1] = 'X';
+        }
+        if (winx())
+        {
+            std::cout << "Gracz 1 wygrał" << std::endl;
+            std::cout << a[0] << " | " << a[1] << " | " << a[2] << std::endl;
+            std::cout << "----------" << std::endl;
+            std::cout << a[3] << " | " << a[4] << " | " << a[5] << std::endl;
+            std::cout << "----------" << std::endl;
+            std::cout << a[6] << " | " << a[7] << " | " << a[8] << std::endl;
+            break;
         }
 
         std::cout << "Player 2's turn:" << std::endl;
@@ -45,28 +143,44 @@ int main(int, char **)
         std::cout << "----------" << std::endl;
         std::cout << a[6] << " | " << a[7] << " | " << a[8] << std::endl;
         std::cin >> o;
-
-        if (a[o - 1] != ' ')
+        if (std::cin.good())
         {
-            std::cerr << "taken" << std::endl;
-            return false;
-        }
-        if (!(o >= 1 && o <= 9))
-        {
-            std::cerr << "wrong input" << std::endl;
-            return false;
-        }
-        else
-        {
+            if (!(o >= 1 && o <= 9))
+            {
+                std::cerr << "wrong input" << std::endl;
+                continue;
+            }
+            if (a[o - 1] != ' ')
+            {
+                std::cerr << "taken" << std::endl;
+                continue;
+            }
 
             a[o - 1] = 'O';
         }
-    }
-    //   std::cout << "\x1b[1;mHell" << std::endl;
 
-    //   for (int i = 0; i < 256; i++)
-    //  {
-    //     std::cout << "\x1b[38;5;" << i << "m " << i;
-    //  }
-    return 0;
+        else
+        {
+            std::cerr << "wrong input" << std::endl;
+            std::cin >> o;
+            continue;
+        }
+
+        if (wino())
+        {
+            std::cout << "Gracz 2 wygrał" << std::endl;
+            std::cout << a[0] << " | " << a[1] << " | " << a[2] << std::endl;
+            std::cout << "----------" << std::endl;
+            std::cout << a[3] << " | " << a[4] << " | " << a[5] << std::endl;
+            std::cout << "----------" << std::endl;
+            std::cout << a[6] << " | " << a[7] << " | " << a[8] << std::endl;
+            break;
+        }
+        //   std::cout << "\x1b[1;mHell" << std::endl;
+
+        //   for (int i = 0; i < 256; i++)
+        //  {
+        //     std::cout << "\x1b[38;5;" << i << "m " << i;
+        //  }
+    }
 }
